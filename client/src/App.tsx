965f6fe9cbc119353f889fa718dd2c84627b5f17
Andy Tseng <anby1611@gmail.com>
2023-02-13 23:18:14 -0800

    Build header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,8 +13,9 @@ function App() {
 	const router = createBrowserRouter(
 		createRoutesFromElements(
 			<Route path="/" element={<HomePages />}>
-				<Route path="createimage" element={<h1>Create Image</h1>} />
-				<Route path="createText" element={<h1>Create Text</h1>} />
+				<Route index element={<h1>123</h1>} />
+				<Route path="create-image" element={<h1>Create Image</h1>} />
+				<Route path="create-text" element={<h1>Create Text</h1>} />
 			</Route>
 		)
 	)
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,31 @@
 import { Link, Outlet } from 'react-router-dom'
+import { logo } from '../assets'
 
 const HomePages = () => {
 	return (
 		<>
-			<header>
+			<header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 				<Link to={'/'}>
-					<img src={'../assets/logo.svg'}></img>
+					<img src={logo} alt="logo" className="w-28 object-contain"></img>
 				</Link>
+				<div>
+					<Link
+						to={'/create-image'}
+						className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+					>
+						Create Image
+					</Link>
+					<Link
+						to={'/create-text'}
+						className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 ml-3 rounded-md"
+					>
+						Create Text
+					</Link>
+				</div>
 			</header>
-			<Outlet />
+			<main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+				<Outlet />
+			</main>
 		</>
 	)
 }
