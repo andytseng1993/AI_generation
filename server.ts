fe28cb246468d1bcb9f98bebd10dcf2e55ddbf36
Andy Tseng <anby1611@gmail.com>
2023-04-13 15:03:55 +0800

    Upload to vercel

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import posts from './routers/api/post'
 import dalle from './routers/api/dalle'
+import path from 'path'
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
@@ -12,4 +13,8 @@ app.use(cors())
 app.use('/api/posts', posts)
 app.use('/api/dalle', dalle)
 
+app.use(express.static(path.join(__dirname, './client/dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'))
+})
 app.listen(port, () => console.log(`Sever started on port ${port}`))
